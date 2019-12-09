b95eae5284549cb9d966d88e38179a4840f0d53f
Leheng620 <llh238704688@gmail.com>
2019-12-09 09:57:06 +0800

    Initialize final project, finish configuring firebase and storing data in appropriate data structre.

---
+++ src/components/home_screen/HomeScreen.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { NavLink, Redirect } from 'react-router-dom';
+import { firestoreConnect } from 'react-redux-firebase';
+import TodoListLinks from './TodoListLinks'
+
+class HomeScreen extends Component {
+
+    render() {
+        if (!this.props.auth.uid) {
+            return <Redirect to="/login" />;
+        }
+
+        return (
+            <div className="dashboard container">
+                <div className="row">
+                    
+                    <div className="col s8">
+                        <div className="banner">
+                            @todo<br />
+                            List Maker
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        auth: state.firebase.auth
+    };
+};
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect(),
+)(HomeScreen);
