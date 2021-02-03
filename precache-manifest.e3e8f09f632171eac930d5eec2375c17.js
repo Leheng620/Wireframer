8f07d4197434d7359c1b8b21b95ab4ddae7d514b
Leheng620 <llh238704688@gmail.com>
2021-02-03 17:59:05 -0500

    project deployed

---
+++ precache-manifest.e3e8f09f632171eac930d5eec2375c17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "241240344854cc6c5334a3d20f7af140",
+    "url": "/Wireframer/index.html"
+  },
+  {
+    "revision": "1123501cf05100c2b677",
+    "url": "/Wireframer/static/css/main.e6edbf56.chunk.css"
+  },
+  {
+    "revision": "75f5df29ab534aedca06",
+    "url": "/Wireframer/static/js/2.5c56f5e4.chunk.js"
+  },
+  {
+    "revision": "1123501cf05100c2b677",
+    "url": "/Wireframer/static/js/main.d85695be.chunk.js"
+  },
+  {
+    "revision": "e16a737dfb0e937537a2",
+    "url": "/Wireframer/static/js/runtime-main.4212764a.js"
+  }
+]);
